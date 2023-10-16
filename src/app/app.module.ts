56ffd7d4e44dd4a2556377377551c8bcfbc9ef0e
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-10-16 06:49:21 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common'
+
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    HttpClientModule, 
+    FormsModule, 
+    DatePipe, 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
