6cbcc45e55047785cd7db3c345a98506e8bee3b2
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-10-16 18:04:53 -0700

    completed user-edit/delete. user done

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent {
+  user: User = new User();
+
+  constructor(
+    private userSvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  save(): void {
+    this.userSvc.change(this.user).subscribe({
+      next: () => {
+        this.router.navigateByUrl("/users");
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  goBack(): void {
+    window.history.back();
+  }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    this.userSvc.get(id).subscribe({
+      next: (res) => {
+        this.user = res;
+      },
+      error: (err) => console.error(err)
+    })
+  }
 
 }
