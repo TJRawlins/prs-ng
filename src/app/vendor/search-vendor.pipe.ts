1a86092930fa8bcc7dc3a5fc80f69510c88e0831
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-10-17 17:05:15 -0700

    completed vendor class, pipe, and service code

--- src/app/vendor/search-vendor.pipe.ts
+++ src/app/vendor/search-vendor.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Vendor } from './vendor.class';
 
 @Pipe({
   name: 'searchVendor'
 })
 export class SearchVendorPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(vends: Vendor[], substr: string = ""): Vendor[] {
+    if(substr === "") return vends;
 
+    let selectedVends: Vendor[] = []
+    for(let v of vends) {
+      if(v.code.toLowerCase().includes(substr.toLowerCase())) {
+        selectedVends.push(v);
+      }
+    }
+    return selectedVends;
+  }
 }
--- src/app/vendor/vendor.service.ts
+++ src/app/vendor/vendor.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { SystemService } from '../core/system.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vendor } from './vendor.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VendorService {
 
-  constructor() { }
+  get url() {return `${this.sysSvc.config.baseurl}/api/users`};
+
+  constructor(
+    private http: HttpClient,
+    private sysSvc: SystemService
+  ) { }
+
+  list(): Observable<Vendor[]> {
+    return this.http.get(`${this.url}`) as Observable<Vendor[]>
+  }
+  get(id: number): Observable<Vendor> {
+    return this.http.get(`${this.url}/${id}`) as Observable<Vendor>
+  }
+  create(vend: Vendor): Observable<Vendor> {
+    return this.http.post(`${this.url}`, vend) as Observable<Vendor>
+  }
+  change(vend: Vendor): Observable<Vendor> {
+    return this.http.put(`${this.url}/${vend.id}`, vend) as Observable<Vendor>
+  }
+  remove(id: number): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`) as Observable<Vendor>
+  }
+
 }
