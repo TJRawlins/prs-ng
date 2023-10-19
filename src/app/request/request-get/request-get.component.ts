b44139f0799311a77d27afe28912e1595bf16ba2
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-10-18 23:01:20 -0700

    added request-get

--- src/app/request/request-get/request-get.component.ts
+++ src/app/request/request-get/request-get.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Request } from '../request.class'
 import { RequestService } from '../request.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/user/user.class';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-request-get',
@@ -11,13 +13,53 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class RequestGetComponent {
   req: Request = new Request();
 
+  userId!: number;
+  user: User = new User();
+
   constructor(
     private reqSvc: RequestService,
+    private userSvc: UserService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
 
   removeRequest(): void {
+    let id = +this.route.snapshot.params['id'];
+    this.reqSvc.remove(id).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/requests')
+      },
+      error: (err) => console.error(err)
+    })
+  }
+
+  goBack(): void {
+    window.history.back();
+  }
+
+  toggleOn: boolean = false;
+  toggleHide(): void {
+    this.toggleOn = !this.toggleOn;
+  }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    this.reqSvc.get(id).subscribe({
+      next: (res) => {
+        this.req = res as Request;
+        this.userId = res.userId as number;
+        
+        this.userSvc.get(this.userId).subscribe({
+          next: (res) => {
+            this.user = res
+            console.debug(this.user)
+          },
+          error: (err) => console.error(err)
+        })
+        
+      },
+      error: (err) => console.error(err)
+    })
     
   }
 
